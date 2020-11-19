a567c8b1d656a2e3ca6a644268e9f8eb70de313b
flower <flower@apap04.com>
2020-11-18 20:02:13 -0500

    re-create createlink func
    Signed-off-by: flower <flower@apap04.com>

--- handlers/go.mod
+++ handlers/go.mod
@@ -3,3 +3,4 @@ module apap04.com/server/handlers
 go 1.15
 
 replace apap04.com/server/models => ../models
+replace apap04.com/server/utils => ../utils
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,11 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"apap04.com/server/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -19,6 +21,26 @@ type Link struct {
 func NotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(404)
 	w.Write([]byte("Slug not found."))
+}
+
+// CreateLink will create a link and add it to the database.
+func CreateLink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// jsonfile, err := os.Open("db.json")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// db, _ := ioutil.ReadAll(jsonfile)
+
+	var m Link
+	err := utils.DecodeJSONBody(w, r, m)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	fmt.Fprintf(w, "log: ", m)
 }
 
 // GetLink will redirect the user to the link.
@@ -41,6 +63,9 @@ func GetLink(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, link.Url, 301)
 		}
 	}
+
+	// If we reach this, then the ID doesn't exist.
+	http.Error(w, "Slug not found.", http.StatusNotFound)
 }
 
 // ReturnLink returns the details of a specific ID. Same as GetLink
@@ -65,5 +90,10 @@ func ReturnLink(w http.ResponseWriter, r *http.Request) {
 		if link.Id == slug {
 			json.NewEncoder(w).Encode(link)
 		}
+	}
+
+	if m == nil {
+		// If we reach this, then the ID doesn't exist.
+		http.Error(w, "Slug not found.", http.StatusNotFound)
 	}
 }
